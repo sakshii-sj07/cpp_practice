d0de3a2f4fc75bd8eced8199049102d715aa6e0f
SAKSHI JADHAV <sakshijadhav8856@gmail.com>
2025-09-17 18:40:22 +0530

    Add files via upload

---
+++ array/rotation.cpp
@@ -0,0 +1,43 @@
+// Rotate array by k positions (left & right)
+
+#include<iostream>
+using namespace std;
+
+void leftRotation(int arr[] , int k , int n) {
+    cout<<"rotated from left side by "<<k<<endl;
+    for (int i = 0; i < n; i++)
+    {
+        cout<<arr[(i + k )% n]<<" ";
+    }
+    cout<<endl;
+}
+
+void rightRotation(int arr[], int k, int n) {
+    cout<<"rotated from right side by "<<k<<endl;
+    for (int i = 0; i < n; i++)
+    {
+        cout<<arr[(i - k + n ) % n]<<" ";
+    }
+    cout<<endl;
+}
+
+int main() {
+    int n;
+    cout<<"Enter elements in array : ";
+    cin>>n;
+
+    int arr[n];
+    cout<<"Enter "<<n<<" elements : ";
+    for (int  i = 0; i < n; i++)
+    {
+        cin>>arr[i];
+    }
+    
+    int k;
+    cout<<"Enter position for rotation : ";
+    cin>>k;
+
+    leftRotation(arr, k , n);
+    rightRotation(arr, k , n);
+    return 0;
+}
